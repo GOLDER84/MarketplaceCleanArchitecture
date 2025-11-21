a811415297d38c010da9af5e2c10c3ef3e815dd3
Aref Zargar <aref.unlimited.84@gmail.com>
2025-11-21 22:43:00 +0330

    IItemService cs class completed.

--- Marketplace.Application/Interfaces/IItemService.cs
+++ Marketplace.Application/Interfaces/IItemService.cs
@@ -1,7 +1,13 @@
 ﻿
+using Marketplace.Domain;
+
 namespace Service;
 
 public interface IItemService
 {
-    
+    Task<string> AddAsync(string name , double price , string description);
+    Task<string> RemoveAsync(int id);
+    Task<string> BuyAsync(string username , int itemId);
+    Task<IReadOnlyList<Item>> GetAllAsync();
+    Task<Item?> GetByIdAsync(int itemId);
 }
