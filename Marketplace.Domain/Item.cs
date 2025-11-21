35e3b17328e4721d042e150270f1c9a025560cfa
Aref Zargar <aref.unlimited.84@gmail.com>
2025-11-21 21:42:09 +0330

    Item cs class completed.

--- Marketplace.Domain/Item.cs
+++ Marketplace.Domain/Item.cs
@@ -1,10 +1,32 @@
 ﻿using System.ComponentModel.DataAnnotations;
+namespace Marketplace.Domain;
 
 public class Item
 {
     [Key]
-    public int ItemId { get; set; }
-    public string Name { get; set; }
+    public int id {get; set;}
     
-    public List<User> Users { get; set; } = new();
+    // static int ID_ITEM_COUNTER = 1;
+    string name {get; set;}
+    public double price {get; set;}
+    string description {get; set;}
+
+    public Item(string name, double price, string description)
+    {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    private Item()
+    {
+        
+    }
+
+    public void UpdateDetails(string name, double price, string description)
+    {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
 }
