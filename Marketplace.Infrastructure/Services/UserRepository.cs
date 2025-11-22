29e620de4466f1bb8bc805491127b7a245647575
Aref Zargar <aref.unlimited.84@gmail.com>
2025-11-22 21:52:16 +0330

    UserRepository cs class completed

--- Marketplace.Infrastructure/Services/UserRepository.cs
+++ Marketplace.Infrastructure/Services/UserRepository.cs
@@ -1,10 +1,53 @@
-﻿using Service.Interfaces.Repsitoreis;
+﻿using Marketplace.Application.Interfaces;
+using Marketplace.Domain;
+using Microsoft.Data.SqlClient.DataClassification;
+using Microsoft.EntityFrameworkCore;
+using Service.Interfaces.Repsitoreis;
 
+namespace Marketplace.Infrastructure.Repositories;
 
 public class UserRepository : IUserRepository
 {
-    public bool AddUserToDataBase(User user)
+    private readonly DatabaseManager _context;
+
+    public UserRepository(DatabaseManager context)  
     {
-        throw new NotImplementedException();
+        _context = context;
+    }
+
+    public async Task<User?> GetUserByUserNameAsync(string username)
+    {
+        return await _context.Users.FirstOrDefaultAsync(u => u.username == username);
+    }
+
+    public async Task<User?> GetUserByIdAsync(int id)
+    {
+        return await _context.Users.FirstOrDefaultAsync(u => u.id == id);
+    }
+
+    public async Task<bool> ExistsUserAsync(string username)
+    {
+        return await _context.Users.AnyAsync(u => u.username == username);
+    }
+
+    public async Task AddUserAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+    }
+
+    public Task UpdateUserAsync(User user)
+    {
+        _context.Users.Update(user);
+        return Task.CompletedTask;
+    }
+
+    public Task SaveAsync()
+    {
+        return _context.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<User>> GetAllUsersAsync()
+    {
+        return await _context.Users.ToListAsync();
     }
 }
