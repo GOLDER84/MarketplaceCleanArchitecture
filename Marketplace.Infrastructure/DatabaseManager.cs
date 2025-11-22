e0c504f45d14fe23aef522eb2d7f104f48e192a4
Aref Zargar <aref.unlimited.84@gmail.com>
2025-11-22 21:56:06 +0330

    DatabaseManager cs file changed and completed

--- Marketplace.Infrastructure/DatabaseManager.cs
+++ Marketplace.Infrastructure/DatabaseManager.cs
@@ -1,29 +1,51 @@
 using Microsoft.EntityFrameworkCore;
+using Marketplace.Domain;
 
-
-
+namespace Marketplace.Infrastructure;
 public class DatabaseManager :DbContext
 {
-    static readonly string connectionString = "Server=localhost; User ID=root; Password=root; Database=test";
+    // static readonly string connectionString = "Server=localhost; User ID=root; Password=Aref@4466; Database=market";
 
     public DbSet<User> Users { get; set; }
     public DbSet<Item> Items { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public DatabaseManager(DbContextOptions<DatabaseManager> options) : base(options)
     {
-        optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
         
-        // optionsBuilder.UseNpgsql("Host=localhost;Database=market;Username=postgres;Password=root");
     }
+    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    // {
+    //     optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    //     
+    //     // optionsBuilder.UseNpgsql("Host=localhost;Database=market;Username=postgres;Password=root");
+    // }
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-
-        modelBuilder.Entity<User>()
-            .HasMany(u => u.Items)
-            .WithMany(i => i.Users)
-            .UsingEntity(j => j.ToTable("UserItems"));
+        
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("Users");
+            entity.HasKey(e => e.id);
+            entity.Property(e => e.id).HasColumnName("id").ValueGeneratedOnAdd();
+            entity.Property(e => e.name).IsRequired().HasMaxLength(50);
+            entity.Property(e => e.username).IsRequired().HasMaxLength(50);
+            entity.Property(e => e.password).IsRequired().HasMaxLength(50);
+            entity.Property(e => e.age).IsRequired();
+            entity.Property(e => e.credit).IsRequired().HasDefaultValue(0);
+            entity.Property(e => e.email).IsRequired().HasMaxLength(100);
+        });
+        
+        modelBuilder.Entity<Item>(entity =>
+        {
+            entity.ToTable("Items");
+            entity.HasKey(e => e.id);
+            entity.Property(e => e.id).HasColumnName("id").ValueGeneratedOnAdd();
+            entity.Property(e => e.name).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.price).IsRequired();
+            entity.Property(e => e.description).IsRequired().HasMaxLength(500);
+        });
     }
     
     public void InitializeDatabase()
