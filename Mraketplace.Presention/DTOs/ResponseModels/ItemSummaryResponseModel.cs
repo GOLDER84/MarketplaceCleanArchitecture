162f5668e566797df75a8ef5045582f4db4c3b47
Aref Zargar <aref.unlimited.84@gmail.com>
2025-11-22 22:01:17 +0330

    All DTOs classes added and completed

---
+++ Mraketplace.Presention/DTOs/ResponseModels/ItemSummaryResponseModel.cs
@@ -0,0 +1,23 @@
+﻿using Marketplace.Domain;
+
+namespace Mraketplace.Presention.DTOs.ResponseModels;
+
+public class ItemSummaryResponseModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public string Description { get; set; }
+
+    public static ItemSummaryResponseModel? FromDomain(Item? item)
+    {
+        if (item == null) return null;
+        return new ItemSummaryResponseModel
+        {
+            Id = item.id,
+            Name = item.name,
+            Price = item.price,
+            Description = item.description
+        };
+    }
+}
