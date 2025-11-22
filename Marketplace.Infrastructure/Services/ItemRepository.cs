f37f6badbd60c015f3db00f7133080c3105a6d60
Aref Zargar <aref.unlimited.84@gmail.com>
2025-11-22 21:59:29 +0330

    ItemRepository cs file added and completed

---
+++ Marketplace.Infrastructure/Services/ItemRepository.cs
@@ -0,0 +1,46 @@
+﻿using Marketplace.Application.Interfaces;
+using Marketplace.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Marketplace.Infrastructure.Repositories;
+
+public class ItemRepository : IItemRepository
+{
+    private readonly DatabaseManager _context;
+
+    public ItemRepository(DatabaseManager context)
+    {
+        _context = context;
+    }
+    public async Task<Item?> GetItemByIdAsync(int id)
+    {
+        return await _context.Items.FirstOrDefaultAsync(i => i.id == id);
+    }
+
+    public async Task<IEnumerable<Item>> GetAllItemAsync()
+    {
+        return await _context.Items.ToListAsync();
+    }
+
+    public async Task AddItemAsync(Item item)
+    {
+        await _context.Items.AddAsync(item);
+    }
+
+    public Task RemoveItemAsync(Item item)
+    {
+        _context.Items.Remove(item);
+        return Task.CompletedTask;
+    }
+
+    public Task BuyItemAsync(Item item)
+    {
+        _context.Items.Update(item);
+        return Task.CompletedTask;
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
