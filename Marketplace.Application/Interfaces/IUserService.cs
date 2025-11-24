fa5b9811489a3b6f345a6216c6823441434efe8a
Aref Zargar <aref.unlimited.84@gmail.com>
2025-11-24 18:32:31 +0330

    LogoutAsync method added

--- Marketplace.Application/Interfaces/IUserService.cs
+++ Marketplace.Application/Interfaces/IUserService.cs
@@ -6,7 +6,8 @@ namespace Service;
 public interface IUserService
 {
     Task<string> RegisterAsync(string name ,string username , string password , int age , double credit , string email);
-    Task<string> LoginAsync(string username , string password);
+    Task<User?> LoginAsync(string username , string password);
+    Task LogoutAsync(); 
     Task<string> EditAsync(string name ,string username , string password , int age , string email); 
     Task<string> AddCreditAsync(string username , double amount);
     Task<User?> GetUserByNameAsync(string name);
