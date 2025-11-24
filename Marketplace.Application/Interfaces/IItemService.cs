397830390bef189c7f45e5417f621f7a9142c2e1
Aref Zargar <aref.unlimited.84@gmail.com>
2025-11-24 18:30:31 +0330

    BuyAsync method changed

--- Marketplace.Application/Interfaces/IItemService.cs
+++ Marketplace.Application/Interfaces/IItemService.cs
@@ -7,7 +7,7 @@ public interface IItemService
 {
     Task<string> AddAsync(string name , double price , string description);
     Task<string> RemoveAsync(int id);
-    Task<string> BuyAsync(string username , int itemId);
+    Task<string> BuyAsync(int itemId);
     Task<IReadOnlyList<Item>> GetAllAsync();
     Task<Item?> GetByIdAsync(int itemId);
 }
